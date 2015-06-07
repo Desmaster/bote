2530f12994458cc3fb697e3592ecdfd1f50cd168
Lorey <leroytjeu@hotmail.com>
2015-06-07 22:41:06 +0200

    Improved setting of color in sprite rendering

--- src/nl/tdegroot/games/bote/common/packet/FilePacket.java
+++ src/nl/tdegroot/games/bote/common/packet/FilePacket.java
@@ -5,8 +5,13 @@ import nl.tdegroot.games.bote.common.ClientListener;
 import nl.tdegroot.games.bote.common.ServerListener;
 
 public class FilePacket implements Packet {
+    Byte[] data;
+
     @Override
     public void onServer(Connection connection, ServerListener serverListener) {
+        //Send data to client
+
+
     }
 
     @Override
